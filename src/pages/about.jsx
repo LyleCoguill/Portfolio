843de94ea8f4e844de7991453a43daa33293fbbe
LyleCoguill <lcoguill@gmail.com>
2023-07-28 11:57:26 -0600

    update images

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -65,30 +65,27 @@ export default function About() {
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I'm a proud husband and father of two. For over two years, I've
+                been fully immersed in the world of software development and
+                crypto, finding a perfect balance between the captivating
+                front-end artwork and the logical complexities of the back-end..
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                From the moment I dove into software development and crypto I
+                have been hooked. The fusion of elegant front-end artwork and
+                the intricacies of back-end logic captivates me, and I find
+                myself endlessly immersed in the dynamic world of technology.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                Beyond coding, I'm deeply passionate about the potential of
+                decentralization in the crypto industry. It's inspiring to see
+                how it brings power to the people.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                For me, technology isn't just a career; it's a mission. I aim to
+                leave a positive impact, whether through innovative solutions,
+                advocating for decentralization, or inspiring the next
+                generation of tech enthusiasts.
               </p>
             </div>
           </div>
